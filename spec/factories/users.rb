bf676fb4e5b563145758d282d3e64d71edcd3e45
Guilherme Silva <guilherme.tr.silva@gmail.com>
2019-10-21 21:24:32 -0300

    Adjustement

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-  factory :user do
-    name { Faker::Name.name }
-    email { Faker::Internet.free_email }
-    password '123456'
-    password_confirmation '123456'
+    factory :user do
+      name { Faker::Name.name }
+      email { Faker::Internet.free_email }
+      password { Faker::Internet.password }
+    end
   end
-end
+  
